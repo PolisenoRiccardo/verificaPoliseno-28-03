318d8d81267ded8c8a599bd5e3bad79e74b0253d
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2025-03-28 12:40:06 +0000

    punto 4

---
+++ poliseno/routes/pages.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+// Percorso al file JSON
+const dataPath = path.join(__dirname, '../data.json');
+
+// Legge i dati delle squadre
+function getUsers() {
+  const jsonData = fs.readFileSync(dataPath);
+  return JSON.parse(jsonData);
+}
+
+var users = getUsers().results;
+
+/* GET users listing. */
+router.get('/people/:email', function(req, res, next) {
+  const userMail = req.params.email;
+  for (i = 0; i < users.length; i++) {
+    if (users[i].email == userMail) {
+      var user = users[i]; }}
+  console.log(userMail)
+  res.render('people', {user: user})
+});
+
+
+
+module.exports = router;
